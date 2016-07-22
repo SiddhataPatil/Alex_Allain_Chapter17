42f15ce0d44c9d6b0881e37d5a790ecfc6afcfd5
Sid <siddhata.s.patil@gmail.com>
2016-07-21 22:16:18 -0700

    Update insert.cpp

--- insert.cpp
+++ insert.cpp
@@ -1,13 +1,3 @@
-/*
- ==============================================================================================================================
- Name        : insert.cpp
- Author      : Siddhata Patil
- Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
- Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
- Question    : Insert a number at root, left and right child in binary tree
- ==============================================================================================================================
- */
- 
 #include <iostream>
 using namespace std;
 
@@ -17,6 +7,7 @@ struct node
     node* right;
     int data;
 };
+
 
 node* insert1 (node *tree, int new_data)
 {
@@ -38,18 +29,31 @@ node* insert1 (node *tree, int new_data)
 
 void print1(node* p1)
 {
-    cout<<"         "<<p1->data<<endl;
-    cout<<p1->left->data;
-    cout<<"                 "<<p1->right->data<<endl;
+    cout<<"                          "<<p1->data<<endl;
+    cout<<"      "<<p1->left->data;
+    cout<<"                                      "<<p1->right->data<<endl;
+    cout<<p1->left->left->data;
+    cout<<"              "<<p1->left->right->data;
+    cout<<"                 "<<p1->right->left->data;
+    cout<<"                          "<<p1->right->right->data;
+
+
 }
 
 int main()
 {
 node* p = NULL;
+node* p1;
+p = insert1 (p, 10);
+p1=p;
+p = insert1 (p, 6);
 p = insert1 (p, 5);
-p = insert1 (p, 3);
 p = insert1 (p, 7);
+p = insert1 (p, 14);
+p = insert1 (p, 18);
+p = insert1 (p, 11);
 print1(p);
+
 system ("pause");
 return 0;
 }
