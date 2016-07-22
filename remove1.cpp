a7fdc8963c99570fbcc1065790298187ecc2e5ee
Sid <siddhata.s.patil@gmail.com>
2016-07-22 08:40:47 -0700

    Create remove1.cpp

---
+++ remove1.cpp
@@ -0,0 +1,99 @@
+/*
+ ==============================================================================================================================
+ Name        : remove.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright ©  Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright ©  Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Remove an element from a binary tree Case 1: It has no children.
+ ==============================================================================================================================
+ */
+ 
+ #include <iostream>
+using namespace std;
+
+struct node
+{
+    node* left;
+    node* right;
+    int data;
+};
+
+
+node* insert1 (node *tree, int new_data)
+{
+    if(tree == NULL)
+    {
+        node *new_tree = new node;
+        new_tree->left=NULL;
+        new_tree->right=NULL;
+        new_tree->data=new_data;
+        return new_tree;
+    }
+    if (new_data < tree->data)
+    {
+        tree->left = insert1(tree->left, new_data);
+    }
+    else
+        tree->right = insert1(tree->right, new_data);
+}
+
+void print1(node* p1)
+{
+    cout<<"                          "<<p1->data<<endl;
+    cout<<"      "<<p1->left->data;
+    cout<<"                                      "<<p1->right->data<<endl;
+    cout<<p1->left->left->data;
+    cout<<"              "<<p1->left->right->data;
+    cout<<"                 "<<p1->right->left->data;
+    cout<<"                          "<<p1->right->right->data;
+}
+
+node* remove1(node* p1, int value)
+{
+    if(p1==NULL)
+    {
+        return NULL;
+    }
+    if(p1->data==value)
+    {
+        if(p1->left==NULL && p1->right==NULL)
+        {
+        cout <<"\nDeleting..."<<p1->data<<endl;
+        p1->data=0;
+        delete p1->left;
+        delete p1->right;
+        delete p1;
+        return NULL;
+        }
+    }
+    else if (p1->data > value)
+    {
+         return remove1(p1->left,value);
+    }
+    else if (p1->data < value)
+    {
+         return remove1(p1->right,value);
+    }
+}
+
+
+int main()
+{
+node* p = NULL;
+node* p1;
+p = insert1 (p, 10);
+p = insert1 (p, 6);
+p = insert1 (p, 5);
+p = insert1 (p, 7);
+p = insert1 (p, 14);
+p = insert1 (p, 18);
+p = insert1 (p, 11);
+print1(p);
+p1=remove1(p, 7);
+print1(p);
+system ("pause");
+return 0;
+}
+
+
+
